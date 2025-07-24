63e34a5d53741e98147b8e3f09fdf475425ce8a2
bharadwajujjwal22 <ujjwalbharadwaj56@gmail.com>
2025-07-25 00:42:36 +0530

    about section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -12,11 +12,11 @@ const AboutPage = () => (
     </div>
     <div className="about-mission-vision-row">
       <div className="about-card">
-        <h2>Our Mission</h2>
+        <h2 className='about-card-title'>Our Mission</h2>
         <p>To empower shoppers by providing a diverse range of quality products, unbeatable value, and a convenient online shopping experience you can trust.</p>
       </div>
       <div className="about-card">
-        <h2>Our Vision</h2>
+        <h2 className="about-card-title">Our Vision</h2>
         <p>To become a leading global e-commerce platform where customers discover, shop, and enjoy products from around the world with confidence and ease.</p>
       </div>
     </div>
@@ -33,7 +33,7 @@ const AboutPage = () => (
     <div className="about-journey-section">
       <h2>Join the Journey</h2>
       <p>Shop. Save. Smile. Experience the joy of effortless online shopping with Uvenza.</p>
-      <Link to="/contact" className="contact-btn">Contact Us</Link>
+      <Link to="/contact" className="contact-btn ">Contact Us</Link>
     </div>
   </div>
 );
